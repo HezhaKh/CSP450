7a57010ba663cf5128dbae8b4a45efe23e07f0ff
Hezhakh <hkhoshnoud@myseneca.ca>
2024-01-25 15:19:27 -0500

    bugs fixed

--- sshdConf.py
+++ sshdConf.py
@@ -6,19 +6,20 @@ dateAndTime: 1/25/24-10:45
 description: This program disables the rootSSH and PasswdSSH
 """
 from os import geteuid as euid
-def createConfigFile():
+
+def createConfigFile():                                         #creat the file and return the file path
     filePath="/etc/ssh/sshd_config.d/10-noRootLogin"
     config="Port 22\n# Auth\nPermitRootLogin no\n# Disble Password\nPasswordAuthentication no\n# challenge-response passsword (could over-ride )\nKbdInteractiveAuthentication no\n"
+    with open(filePath, "w") as file:
+        file.write(config)
+    return filePath
 
-def main():
-    try:
-        if euid == 0:
-            createConfigFile()
-            print(f"The {createConfigFile.filePath()} was created.")
-        else:
+def main():                                                     #check for the privilages and 
+    if euid()==0:
+            thePath=createConfigFile()
+            print(f"The [{thePath}] was created.")
+    else:
             print("please run the program with the root privilages.")
-    except:
-        print("please run the program with the root privilages.")
 
 if __name__=="__main__":
     main()
