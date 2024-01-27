ab3f4d622a7e3283fb40d04dc7bf0722e72fc568
Hezhakh <hkhoshnoud@myseneca.ca>
2024-01-26 23:50:12 -0500

    Initial push

---
+++ ipRouter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#fileName: ipRouter.sh
+#author: Hezha
+#dateAndTime: 1/25/24-15:48
+#description: This program take destination ip address and your router and it to the table.
+
+#check if the program has root privilage
+if [["$EUID" -ne 0]]; then
+    echo "Please run the script with root privilages."
+    exit 1
+fi
+
+#error handler
+if [["$#" -ne 2]]; then
+    echo "Usage: $0 [ip1] [ip2]"
+    exit 1
+fi
+
+ip1="$1"
+ip2="$2"
+
+ip route add "$ip1" via "$ip2"
+echo "Route added: $ip1 via $ip2"
